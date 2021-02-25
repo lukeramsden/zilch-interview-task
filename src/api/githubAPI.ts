95de11181c812bf49c96dce698b20a6f08f0a9ba
Luke Ramsden <luke@payzilch.com>
2021-02-25 10:00:49 +0000

    Cut down GitHub API

--- src/api/githubAPI.ts
+++ src/api/githubAPI.ts
@@ -1,5 +1,4 @@
-import axios from 'axios'
-import parseLink, { Links } from 'parse-link-header'
+import { Links } from 'parse-link-header'
 
 export interface Label {
   id: number
@@ -45,66 +44,12 @@ export interface IssuesResult {
   issues: Issue[]
 }
 
-const isLastPage = (pageLinks: Links) => {
-  return (
-    Object.keys(pageLinks).length === 2 && pageLinks.first && pageLinks.prev
-  )
-}
-
-const getPageCount = (pageLinks: Links) => {
-  if (!pageLinks) {
-    return 0
-  }
-  if (isLastPage(pageLinks)) {
-    return parseInt(pageLinks.prev.page, 10) + 1
-  } else if (pageLinks.last) {
-    return parseInt(pageLinks.last.page, 10)
-  } else {
-    return 0
-  }
-}
-
-export async function getIssues(
+export const createIssuesUrl = (
   org: string,
   repo: string,
   page = 1
-): Promise<IssuesResult> {
-  const url = `https://api.github.com/repos/${org}/${repo}/issues?per_page=25&page=${page}`
+) => `https://api.github.com/repos/${org}/${repo}/issues?per_page=25&page=${page}`;
 
-  try {
-    const issuesResponse = await axios.get<Issue[]>(url)
-    let pageCount = 0
-    const pageLinks = parseLink(issuesResponse.headers.link)
+export const createRepoDetailsUrl = (org: string, repo: string) => `https://api.github.com/repos/${org}/${repo}`;
 
-    if (pageLinks !== null) {
-      pageCount = getPageCount(pageLinks)
-    }
-
-    return {
-      pageLinks,
-      pageCount,
-      issues: issuesResponse.data
-    }
-  } catch (err) {
-    throw err
-  }
-}
-
-export async function getRepoDetails(org: string, repo: string) {
-  const url = `https://api.github.com/repos/${org}/${repo}`
-
-  const { data } = await axios.get<RepoDetails>(url)
-  return data
-}
-
-export async function getIssue(org: string, repo: string, number: number) {
-  const url = `https://api.github.com/repos/${org}/${repo}/issues/${number}`
-
-  const { data } = await axios.get<Issue>(url)
-  return data
-}
-
-export async function getComments(url: string) {
-  const { data } = await axios.get<Comment[]>(url)
-  return data
-}
+export const createIssueUrl = (org: string, repo: string, number: number) => `https://api.github.com/repos/${org}/${repo}/issues/${number}`;
